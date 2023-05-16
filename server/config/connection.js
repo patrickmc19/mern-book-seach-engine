b7e670cd474fc39426991526e70ea452cb1ec5cc
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-16 15:17:44 -0700

    created mongoose connection parameters

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    dbName: "book_db",
+  }
+);
 
 module.exports = mongoose.connection;
