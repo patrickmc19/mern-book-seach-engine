bd4ec0f56f4ecc9de0b39b11dc2b22a887f44c3c
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-18 22:20:56 -0700

    bookInput in typeDefs server schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: BookInput): User
+        saveBook(input: bookInput): User
         removeBook(bookId: ID!): User
     }
 
