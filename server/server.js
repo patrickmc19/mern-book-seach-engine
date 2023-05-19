eb011b1effb65b75e376fa4b8924e46f96dc92f4
Patrick McKnight <patrickgmcknight@gmail.com>
2023-05-18 22:32:52 -0700

    updated server.js file, url encoded extended = true

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { authMiddleware } = require('./utils/auth');
-const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
+const app = express();
 const PORT = process.env.PORT || 3001;
-
 // define the Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
@@ -14,20 +15,17 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 // create a new Apollo server and pass our schema data
 const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
-
-  // if we're in production, serve client/build as static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
 
   db.once('open', () => {
     app.listen(PORT, () => {
